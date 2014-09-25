e3ac2391a9a8bd5a53cda3d32006b179bd97426f
Taro L. Saito <leo@xerial.org>
2014-09-25 22:59:51 +0900

    Add Hive query AST parser

---
+++ app/controllers/HiveQuery.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import controllers.Query._
+import org.antlr.runtime.{CommonTokenStream, ANTLRStringStream}
+import org.apache.hadoop.hive.ql.parse.{ParseException, ParseDriver}
+import play.api._
+import play.api.mvc._
+import play.api.mvc.BodyParsers.parse
+
+
+/**
+ *
+ */
+object HiveQuery extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("hello Hive query"))
+  }
+
+  def validate = Action(parse.text) { request =>
+    val sql = request.body
+
+    val driver = new ParseDriver()
+    try {
+      val ast = driver.parse(sql)
+      Logger.debug(ast.dump())
+      ast.dump()
+      Ok(views.html.index(s"Your query: ${sql}"))
+    }
+    catch {
+      case e:ParseException =>
+        // Extract error parts
+        Ok(views.html.index(s"parse exception: ${e.getMessage}"))
+    }
+
+
+  }
+
+}
---
+++ app/controllers/PrestoQuery.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import com.facebook.presto.sql.parser.{ParsingException, SqlParser}
+import controllers.Query._
+import play.api._
+import play.api.mvc._
+import play.api.mvc.BodyParsers.parse
+/**
+ *
+ */
+object PrestoQuery extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("hello query"))
+  }
+
+  def validate = Action(parse.text) { request =>
+    val sql = request.body
+    val sqlParser = new SqlParser
+    try {
+      val statement = sqlParser.createStatement(sql)
+      Ok(views.html.index(s"Your query: ${sql}"))
+    }
+    catch {
+      case e : ParsingException =>
+        BadRequest(views.html.index(s"syntax error: ${e.getErrorMessage}"))
+    }
+  }
+
+}
